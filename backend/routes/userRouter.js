ada67d000454d4d486be994ba7386388efe791d2
Juan Yu <sigrace0417@gmail.com>
2026-09-15 10:13:26 +0300

    add sign up and log out

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -2,9 +2,41 @@ import { Router } from 'express'
 import { compare, hash } from 'bcrypt'
 import jwt from 'jsonwebtoken'
 import { pool } from '../helper/db.js'
+import { auth } from '../helper/auth.js'
 
 const { sign } = jwt
 const router = Router()
+
+//users/signup
+router.post('/signup', async (req, res, next) => {
+  try {
+    const username = req.body?.user?.username
+    const email = req.body.user?.email?.trim().toLowerCase()
+    const password = req.body.user?.password
+    if (!username || !email || !password) {
+      const error = new Error('Username, email and password are required')
+      error.status = 400
+      return next(error)
+    }
+    const passwordRegex = /^(?=.*[A-Z])(?=.*\d).{8,}$/
+    if (!passwordRegex.test(password)) {
+      const error = new Error(
+      'Password must be at least 8 characters long and contain at least one uppercase letter and one number'
+      )
+      error.status = 400
+      return next(error)
+   }
+    const hashedPassword = await hash(password, 10)
+    const result = await pool.query(
+      `INSERT INTO users (email, username, password_hash) VALUES ($1, $2, $3) RETURNING user_id, email,username`,
+      [email, username, hashedPassword]
+    )
+
+    return res.status(201).json(result.rows[0])
+  } catch (error) {
+    return next(error)
+  }
+})
 
 //users/login
 router.post('/login', async (req, res, next) => {
@@ -36,4 +68,10 @@ router.post('/login', async (req, res, next) => {
     return next(error)
   }
 })
+
+//users/logout
+router.post('/logout', auth, (req, res) => {
+  return res.status(200).json({ message: 'Logged out successfully' })
+})
+
 export default router
