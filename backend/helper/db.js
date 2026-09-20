14362f63a0bbd81fb30dcb6ce7ef87d7bfe261db
ecedevere <sheykina.mariia@gmail.com>
2026-09-20 19:39:19 +0300

    debugged .env configuration

--- backend/helper/db.js
+++ backend/helper/db.js
@@ -1,5 +1,19 @@
 import pkg from 'pg'
-import 'dotenv/config'
+import dotenv from 'dotenv'
+dotenv.config({ path: '../.env' })
+
+/*
+for debugging the DB connection:
+
+make sure that the values printed in the terminal match the values in your .env file in the filmio folder
+
+console.log('DB config:', {
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
+})
+*/
 
 const environment = process.env.NODE_ENV || 'development'
 const { Pool } = pkg
